5c8b993104c289142854f7b90bcdbb51a946db1b
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-07-12 17:10:24 +0530

    Added logo and sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import SidebarComponent from "./SidebarComponent";
 
 function App() {
   return (
     <div className="App">
-      Heelo hehe
+      <div className="sidebar">
+        <SidebarComponent />
+      </div>
+      <div className="main-section">
+        <h4>Under Construction</h4>
+      </div>
     </div>
   );
 }
---
+++ src/SidebarComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsTwitter} from 'react-icons/bs'
+
+function SidebarComponent() {
+  return (
+    <div className='sidebar-component'>
+      <BsTwitter size={30} />
+      
+    </div>
+  )
+}
+
+export default SidebarComponent;
